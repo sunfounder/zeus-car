446db54576c60174ff552f06b003b2888a5d6a01
Cavon <cavonxx@gmail.com>
2022-04-29 18:32:23 +0800

    tide up car_control

--- Arduino_Car/car_control.cpp
+++ Arduino_Car/car_control.cpp
@@ -5,12 +5,23 @@
 
 #define MOTOR_POWER_MIN 28  // 28/255
 
-bool motorDirection[4] = {1, 0, 0, 1};
-uint8_t motorPins[8] = {3, 4, 5, 6, 7, 8, 9, 10};
+#define MOTOR_DIRECTION_0 1
+#define MOTOR_DIRECTION_1 0
+#define MOTOR_DIRECTION_2 0
+#define MOTOR_DIRECTION_3 1
+#define MOTOR_PIN_0_A 3
+#define MOTOR_PIN_0_B 4
+#define MOTOR_PIN_1_A 5
+#define MOTOR_PIN_1_B 6
+#define MOTOR_PIN_2_A 7
+#define MOTOR_PIN_2_B 8
+#define MOTOR_PIN_3_A 9
+#define MOTOR_PIN_3_B 10
 
-float kP = 0.8;
-float kI = 0.0;
-float kD = 20;
+#define KP 0.8
+#define KI 0.0
+#define KD 20.0
+
 int32_t _lastError = 0;
 int32_t errorIntegral = 0;
 
@@ -18,10 +29,22 @@ int16_t originHeading;
 
 void carBegin() {
   SoftPWMBegin();
-  for (uint8_t i = 0; i < 8; i++) {
-    SoftPWMSet(motorPins[i], 0);
-    SoftPWMSetFadeTime(motorPins[i], 100, 100);
-  }
+  SoftPWMSet(MOTOR_PIN_0_A, 0);
+  SoftPWMSet(MOTOR_PIN_0_B, 0);
+  SoftPWMSet(MOTOR_PIN_1_A, 0);
+  SoftPWMSet(MOTOR_PIN_1_B, 0);
+  SoftPWMSet(MOTOR_PIN_2_A, 0);
+  SoftPWMSet(MOTOR_PIN_2_B, 0);
+  SoftPWMSet(MOTOR_PIN_3_A, 0);
+  SoftPWMSet(MOTOR_PIN_3_B, 0);
+  SoftPWMSetFadeTime(MOTOR_PIN_0_A, 100, 100);
+  SoftPWMSetFadeTime(MOTOR_PIN_0_B, 100, 100);
+  SoftPWMSetFadeTime(MOTOR_PIN_1_A, 100, 100);
+  SoftPWMSetFadeTime(MOTOR_PIN_1_B, 100, 100);
+  SoftPWMSetFadeTime(MOTOR_PIN_2_A, 100, 100);
+  SoftPWMSetFadeTime(MOTOR_PIN_2_B, 100, 100);
+  SoftPWMSetFadeTime(MOTOR_PIN_3_A, 100, 100);
+  SoftPWMSetFadeTime(MOTOR_PIN_3_B, 100, 100);
   compassBegin();
   carResetHeading();
 }
@@ -38,31 +61,104 @@ void carLeftBackward()  { carSetMotors(   0, -100,    0, -100); }
 void carRightBackward() { carSetMotors(-100,    0, -100,    0); }
 void carStop()          { carSetMotors(   0,    0,    0,    0); }
 
-void carSetMotor(uint8_t motor, int8_t power) {
-  uint8_t a = motor * 2;
-  uint8_t b = motor * 2 + 1;
-  bool dir = power > 0;
-  int8_t newPower = 0;
-  if (motorDirection[motor]) dir = !dir;
+// void carSetMotor(uint8_t motor, int8_t power) {
+//   bool dir = power > 0;
+//   int8_t newPower = 0;
 
-  if (power == 0) {
-    newPower = 0;
-  } else {
-    newPower = map(abs(power), 0, 100, MOTOR_POWER_MIN, 255);
-  }
+//   if (power == 0) {
+//     newPower = 0;
+//   } else {
+//     newPower = map(abs(power), 0, 100, MOTOR_POWER_MIN, 255);
+//   }
+//   switch (motor) {
+//     case 0:
+//       if (MOTOR_DIRECTION_0) dir = !dir;
+//       SoftPWMSet(MOTOR_PIN_0_A,  dir * newPower);
+//       SoftPWMSet(MOTOR_PIN_0_B, !dir * newPower);
+//       break;
+//     case 1:
+//       if (MOTOR_DIRECTION_1) dir = !dir;
+//       SoftPWMSet(MOTOR_PIN_1_A,  dir * newPower);
+//       SoftPWMSet(MOTOR_PIN_1_B, !dir * newPower);
+//       break;
+//     case 2:
+//       if (MOTOR_DIRECTION_2) dir = !dir;
+//       SoftPWMSet(MOTOR_PIN_2_A,  dir * newPower);
+//       SoftPWMSet(MOTOR_PIN_2_B, !dir * newPower);
+//       break;
+//     case 3:
+//       if (MOTOR_DIRECTION_3) dir = !dir;
+//       SoftPWMSet(MOTOR_PIN_3_A,  dir * newPower);
+//       SoftPWMSet(MOTOR_PIN_3_B, !dir * newPower);
+//       break;
+//   }
+// }
 
-  SoftPWMSet(motorPins[a],  dir * newPower);
-  SoftPWMSet(motorPins[b], !dir * newPower);
+// void carSetMotors(int8_t power1, int8_t power2, int8_t power3, int8_t power4) {
+//   carSetMotor(0, power1);
+//   carSetMotor(1, power2);
+//   carSetMotor(2, power3);
+//   carSetMotor(3, power4);
+// }
+
+void carSetMotors(int8_t power0, int8_t power1, int8_t power2, int8_t power3) {
+  bool dir0, dir1, dir2, dir3;
+  int8_t newPower0, newPower1, newPower2, newPower3;
+
+  dir0 = power0 > 0;
+  dir1 = power1 > 0;
+  dir2 = power2 > 0;
+  dir3 = power3 > 0;
+
+  if (MOTOR_DIRECTION_0) dir0 = !dir0;
+  if (MOTOR_DIRECTION_1) dir1 = !dir1;
+  if (MOTOR_DIRECTION_2) dir2 = !dir2;
+  if (MOTOR_DIRECTION_3) dir3 = !dir3;
+
+  if (power0 == 0) newPower0 = 0;
+  else newPower0 = map(abs(power0), 0, 100, MOTOR_POWER_MIN, 255);
+  if (power1 == 0) newPower1 = 0;
+  else newPower1 = map(abs(power1), 0, 100, MOTOR_POWER_MIN, 255);
+  if (power2 == 0) newPower2 = 0;
+  else newPower2 = map(abs(power2), 0, 100, MOTOR_POWER_MIN, 255);
+  if (power3 == 0) newPower3 = 0;
+  else newPower3 = map(abs(power3), 0, 100, MOTOR_POWER_MIN, 255);
+
+  SoftPWMSet(MOTOR_PIN_0_A,  dir0 * newPower0);
+  SoftPWMSet(MOTOR_PIN_0_B, !dir0 * newPower0);
+  SoftPWMSet(MOTOR_PIN_1_A,  dir1 * newPower1);
+  SoftPWMSet(MOTOR_PIN_1_B, !dir1 * newPower1);
+  SoftPWMSet(MOTOR_PIN_2_A,  dir2 * newPower2);
+  SoftPWMSet(MOTOR_PIN_2_B, !dir2 * newPower2);
+  SoftPWMSet(MOTOR_PIN_3_A,  dir3 * newPower3);
+  SoftPWMSet(MOTOR_PIN_3_B, !dir3 * newPower3);
 }
 
-void carSetMotors(int8_t power1, int8_t power2, int8_t power3, int8_t power4) {
-  carSetMotor(0, power1);
-  carSetMotor(1, power2);
-  carSetMotor(2, power3);
-  carSetMotor(3, power4);
-}
+// void carMove(int16_t angle, int8_t power, int8_t rot) {
+//   int8_t power_0, power_1, power_2, power_3;
+//   angle += 90;
+//   // Offset angle as 0 to the front
+//   float rad = angle * PI / 180;
 
-void carMove(int16_t angle, int8_t power, int8_t rot) {
+//   power /= sqrt(2);
+//   // Calculate 4 wheel
+//   power_0 = (power * sin(rad) - power * cos(rad)) * 0.5 - rot * 0.5;
+//   power_1 = (power * sin(rad) + power * cos(rad)) * 0.5 + rot * 0.5;
+//   power_2 = (power * sin(rad) - power * cos(rad)) * 0.5 + rot * 0.5;
+//   power_3 = (power * sin(rad) + power * cos(rad)) * 0.5 - rot * 0.5;
+//   // Serial.print("power: ");
+//   // Serial.print(power_0);
+//   // Serial.print(" ");
+//   // Serial.print(power_1);
+//   // Serial.print(" ");
+//   // Serial.print(power_2);
+//   // Serial.print(" ");
+//   // Serial.println(power_3);
+
+//   carSetMotors(power_0, power_1, power_2, power_3);
+// }
+
+void _carMove(int16_t angle, int8_t power, int8_t rot, bool drift) {
   int8_t power_0, power_1, power_2, power_3;
   angle += 90;
   // Offset angle as 0 to the front
@@ -70,10 +166,18 @@ void carMove(int16_t angle, int8_t power, int8_t rot) {
 
   power /= sqrt(2);
   // Calculate 4 wheel
-  power_0 = (power * sin(rad) - power * cos(rad)) * 0.6 - rot * 0.4;
-  power_1 = (power * sin(rad) + power * cos(rad)) * 0.6 + rot * 0.4;
-  power_2 = (power * sin(rad) - power * cos(rad)) * 0.6 + rot * 0.4;
-  power_3 = (power * sin(rad) + power * cos(rad)) * 0.6 - rot * 0.4;
+  if (drift) {
+    power_0 = (power * sin(rad) - power * cos(rad)) * 0.5;
+    power_1 = (power * sin(rad) + power * cos(rad)) * 0.5;
+    power_2 = (power * sin(rad) - power * cos(rad)) * 0.5 + rot * 0.5 * 2;
+    power_3 = (power * sin(rad) + power * cos(rad)) * 0.5 - rot * 0.5 * 2;
+  } else {
+    power_0 = (power * sin(rad) - power * cos(rad)) * 0.5 - rot * 0.5;
+    power_1 = (power * sin(rad) + power * cos(rad)) * 0.5 + rot * 0.5;
+    power_2 = (power * sin(rad) - power * cos(rad)) * 0.5 + rot * 0.5;
+    power_3 = (power * sin(rad) + power * cos(rad)) * 0.5 - rot * 0.5;
+  }
+
   // Serial.print("power: ");
   // Serial.print(power_0);
   // Serial.print(" ");
@@ -86,58 +190,42 @@ void carMove(int16_t angle, int8_t power, int8_t rot) {
   carSetMotors(power_0, power_1, power_2, power_3);
 }
 
-void carMove2(int16_t angle, int8_t power, int8_t rot) {
-  int16_t heading;
-  int32_t error;
-  int32_t offset;
+// void carMove(int16_t angle, int8_t power, int8_t rot, bool drift) {
+//   int16_t heading;
+//   int32_t error;
+//   int32_t offset;
 
-  if (rot == 0) {
-    heading = compassReadAngle();
-    error = heading - originHeading;
-    // convert -360 to 360 to -180 to 180
-    if (error > 180) {
-      error -= 360;
-    } else if (error < -180) {
-      error += 360;
-    }
+//   if (rot == 0) {
+//     heading = compassReadAngle();
+//     error = heading - originHeading;
+//     // convert -360 to 360 to -180 to 180
+//     if (error > 180) {
+//       error -= 360;
+//     } else if (error < -180) {
+//       error += 360;
+//     }
 
-    // offset += kP * error + kI * errorIntegral + kD * (_lastError - error);
-    offset += kP * error + kI * errorIntegral + kD * (error - _lastError);
+//     // offset += KP * error + KI * errorIntegral + KD * (_lastError - error);
+//     offset += KP * error + KI * errorIntegral + KD * (error - _lastError);
   
-    // Serial.print("originHeading:");
-    // Serial.print(originHeading);
-    // Serial.print(",heading:");
-    // Serial.print(heading);
-    // Serial.print(',');
-    Serial.print("error:");
-    Serial.print(error);
-    Serial.print(",rot:");
-    Serial.println(offset);
+//     // Serial.print("originHeading:");
+//     // Serial.print(originHeading);
+//     // Serial.print(",heading:");
+//     // Serial.print(heading);
+//     // Serial.print(',');
+//     // Serial.print("error:");
+//     // Serial.print(error);
+//     // Serial.print(",rot:");
+//     // Serial.println(offset);
 
-    rot += max(-100, min(100, offset));
-    errorIntegral += error;
-    _lastError = error;
-  }
-  carMove(angle, power, rot);
-}
+//     rot += max(-100, min(100, offset));
+//     errorIntegral += error;
+//     _lastError = error;
+//   }
+//   _carMove(angle, power, rot, drift);
+// }
 
-void carMoveFieldCentric(int16_t angle, int8_t power, int8_t rot) {
-  uint16_t heading;
-  heading = compassReadAngle();
-  int16_t fcAngle = angle - heading + originHeading;
-  // Serial.print("originHeading:");
-  // Serial.print(originHeading);
-  // Serial.print(",heading:");
-  // Serial.print(heading);
-  // Serial.print(",fcAngle:");
-  // Serial.print(fcAngle);
-  // Serial.print(",fieldAngle:");
-  // Serial.println(originHeading-angle);
-
-  carMove(fcAngle, power, rot);
-}
-
-void carMove3(int16_t angle, int8_t power, int16_t heading) {
+void carMove(int16_t angle, int8_t power, int16_t heading, bool drift) {
   int16_t currentHeading;
   int32_t error;
   int32_t offset;
@@ -153,43 +241,34 @@ void carMove3(int16_t angle, int8_t power, int16_t heading) {
     error += 360;
   }
 
-  // offset += kP * error + kI * errorIntegral + kD * (_lastError - error);
-  offset += kP * error + kI * errorIntegral + kD * (error - _lastError);
-
+  // Serial.print("heading:");
+  // Serial.print(heading);
   // Serial.print("tempHeading:");
   // Serial.print(tempHeading);
-  // Serial.print(",currentHeading:");
-  // Serial.print(currentHeading);
-  // Serial.print(',');
   // Serial.print("error:");
-  // Serial.print(error);
-  // Serial.print(",rot:");
-  // Serial.println(offset);
+  // Serial.println(error);
+  offset += KP * error + KI * errorIntegral + KD * (error - _lastError);
 
   rot += max(-100, min(100, offset));
   errorIntegral += error;
   _lastError = error;
 
-  currentHeading = compassReadAngle();
+  // currentHeading = compassReadAngle();
   int16_t fcAngle = angle - currentHeading + originHeading;
-  Serial.print("originHeading:");
-  Serial.print(originHeading);
-  Serial.print(",currentHeading:");
-  Serial.print(currentHeading);
-  Serial.print(",fcAngle:");
-  Serial.print(fcAngle);
-  Serial.print(",fieldAngle:");
-  Serial.println(originHeading-angle);
+  // Serial.print(",originHeading:");
+  // Serial.print(originHeading);
+  // Serial.print(",currentHeading:");
+  // Serial.print(currentHeading);
+  // Serial.print(",fcAngle:");
+  // Serial.print(fcAngle);
+  // Serial.print(",fieldAngle:");
+  // Serial.println(originHeading-angle);
 
-  carMove(fcAngle, power, rot);
+  _carMove(fcAngle, power, rot, drift);
 }
 
 void carResetHeading() {
   for (uint8_t i = 0; i < AVERAGE_FILTER_SIZE; i++) {
     originHeading = compassReadAngle();
   }
-}
-
-void carHeading() {
-  return originHeading;
 }
--- Arduino_Car/car_control.h
+++ Arduino_Car/car_control.h
@@ -15,8 +15,8 @@
 // [3]-------[2]
 
 void carBegin();
-void carSetMotor(uint8_t motor, int8_t power);
-void carSetMotors(int8_t power1, int8_t power2, int8_t power3, int8_t power4);
+// void carSetMotor(uint8_t motor, int8_t power);
+void carSetMotors(int8_t power0, int8_t power1, int8_t power2, int8_t power3);
 void carForward();
 void carBackward();
 void carTurnLeft();
@@ -28,11 +28,9 @@ void carRightBackward();
 void carLeft();
 void carRight();
 void carStop();
-void carMove(int16_t angle, int8_t power, int8_t rot);
-void carMove2(int16_t angle, int8_t power, int8_t rot);
-void carMoveFieldCentric(int16_t angle, int8_t power, int8_t rot);
-void carMove3(int16_t angle, int8_t power, int16_t heading);
+void _carMove(int16_t angle, int8_t power, int8_t rot, bool drift=false);
+// void carMove(int16_t angle, int8_t power, int8_t rot, bool drift=false);
+void carMove(int16_t angle, int8_t power, int16_t heading, bool drift=false);
 void carResetHeading();
-void carHeading();
 
 #endif // __CAR_CONTROL_H__
